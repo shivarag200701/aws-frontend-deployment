faac7ab67f8da779d8b9f1a4233f054161f18f1d
Shiva Raghav <shivaraghav200701@gmail.com>
2025-10-15 11:12:45 -0500

    changed bucket name from ARN to actual name

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React this is a test deployment again</h1>
+      <h1>Vite + React this is a test deployment again and again</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
